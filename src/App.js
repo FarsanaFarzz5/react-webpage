dc3cb20aee7cb81a63f338e44d2720b36e7ad6ad
farzz5 <farsanafarzz5580@gmail.com>
2024-05-23 11:48:15 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Task1 from './components/task1';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+    <Task1 />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/task1.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function Task1() {
+  return (
+    <div>
+    <section class="first">
+         
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+        <div class="one">
+       <h1 >Material Kit</h1> 
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+       <button class="buttono"><b>Get started</b></button>
+       <button class="buttont"><b>Read More</b></button>
+ 
+       </div>
+       </section>
+    </div>
+  )
+}
+
+export default Task1
